d45e22836be2acd100ce4ac03badf7a77e8423d1
김정민 <raon37910@gmail.com>
2024-11-23 18:41:13 +0900

    Initial Commit

---
+++ src/test/java/com/raon/DemoBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
